878b5fc77d06e8c56202390226943a68d8c61aef
lourencoatanasio <lourencodiogo1234@gmail.com>
2024-04-19 15:16:27 +0100

    ex07 reviewed

--- module07/ex02/srcs/main.cpp
+++ module07/ex02/srcs/main.cpp
@@ -2,39 +2,46 @@
 // Created by ldiogo on 3/21/24.
 //
 
-#include "Array.hpp"
+#include "../inc/Array.hpp"
 #include <ctime>
 #include <cstdlib>
 #include <iostream>
 
-#define MAX_VAL 750
+#define MAX_VAL 10000
 int main(int, char**)
 {
 	Array<int> numbers(MAX_VAL);
 	int* mirror = new int[MAX_VAL];
 	srand(time(NULL));
-	for (int i = 0; i < MAX_VAL; i++)
+	for (int i = 0; i < MAX_VAL; i++) // fill the array with random numbers
 	{
 		const int value = rand();
 		numbers[i] = value;
 		mirror[i] = value;
 	}
-	//SCOPE
-	{
+
+	{ // create a scope
 		Array<int> tmp = numbers;
 		Array<int> test(tmp);
+		for (int i = 0; i < MAX_VAL; i++) // check if the copy constructor works
+		{
+			if (tmp[i] != mirror[i])
+			{
+				std::cerr << "didn't save the same value!!" << std::endl;
+				return 1;
+			}
+		}
 	}
 
-	for (int i = 0; i < MAX_VAL; i++)
+	for (int i = 0; i < MAX_VAL; i++) // check if the values are still the same
 	{
-		std::cout << numbers[i] << std::endl;
 		if (mirror[i] != numbers[i])
 		{
 			std::cerr << "didn't save the same value!!" << std::endl;
 			return 1;
 		}
 	}
-	try
+	try // try to access an element out of the limits
 	{
 		numbers[-2] = 0;
 	}
@@ -42,7 +49,7 @@ int main(int, char**)
 	{
 		std::cerr << e.what() << '\n';
 	}
-	try
+	try // try to access an element in limits
 	{
 		numbers[MAX_VAL - 1] = 0;
 	}
@@ -51,7 +58,7 @@ int main(int, char**)
 		std::cerr << e.what() << '\n';
 	}
 
-	for (int i = 0; i < MAX_VAL; i++)
+	for (int i = 0; i < MAX_VAL; i++) 	// fill the array with random numbers
 	{
 		numbers[i] = rand();
 	}
